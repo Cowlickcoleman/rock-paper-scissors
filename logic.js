061f1588d67de43c6b3daa9a6d8fad9aa9fdd4c9
Cowlickcoleman <roanoak.co@outlook.com>
2019-09-28 18:32:51 +0700

    overhauled js

--- logic.js
+++ logic.js
@@ -1,103 +1,104 @@
+// link DOM elements to js logic
+
+var btnRock = document.querySelector('#rock');
+var btnPaper = document.querySelector('#paper');
+var btnScissors = document.querySelector('#scissors');
+var scoreDialogue = document.querySelector('#scoreDialogue');
+var scoreBoard = document.querySelector('#scoreBoard');
+
+// setup the game
+scoreBoard.textContent='Player: 0 Computer: 0';
+scoreDialogue.textContent='The original dispute settler. Choose an option to begin!';
 const combatOptions = ['ROCK','PAPER','SCISSORS'];
-        var computerScore = 0;
-        var playerScore = 0;
 
-        function playGame(numOfRounds){
-            do {
-                var playerPlay = getPlayerSelection();
-                var computerPlay = getComputerSelection();
-                var result = determineWinner(playerPlay,computerPlay);
-                updateScore(result);
-                // if (result !== 0) {
-                //     numOfRounds--;
-                // }
-                alert(`Best of 5. The current score is Player: ${playerScore} vs. Computer ${computerScore}`);
-            } while (playerScore < (numOfRounds/2) && computerScore < (numOfRounds/2));
+var computerScore = 0;
+var playerScore = 0;
+var numOfRounds = 5;
+var currentRound = 0;
 
-            if (playerScore > computerScore) {
-                alert('Congratulations! You win!')
-            } else {
-                alert('you lose.')
-            };
-            
-            if (confirm("play again?")){
-                clearScores()
-                playGame(5)
-            } else {
-                alert("good game. see ya!")
-            };
-        };
-
-    // --------------------------- FUNCTIONS -------------------
-        // button interactivity
-
-
-
-    
-        // Generate a random computer selection
-            function getComputerSelection (computerSelection) {
-                var computerSelection = combatOptions[Math.floor(Math.random()*3)];
-                return computerSelection;
-            };
-
-        // prompt the user for a selection
-            function getPlayerSelection () {
-                document.getElementById('rock').addEventListener("click",playerSelection="ROCK");
-                document.getElementById('paper').addEventListener("click",playerSelection="PAPER");
-                document.getElementById('scissors').addEventListener("click",playerSelection="SCISSORS");
-            };  
-
-            
-
-                // **OLD CODE - Used for Prompt vs Button**
-                // var playerSelection = prompt('Rock, Paper, or Scissors?',).toUpperCase();
-                //     if(playerSelection === "ROCK" || playerSelection === "PAPER" || playerSelection === "SCISSORS") {
-                //     return playerSelection;
-                //     }  
-                //     alert("yo, that's not an option... try again.");
-                // getPlayerSelection();
         
-        // compare the selections and determins a winner
-            function determineWinner (playerSelection, computerSelection) {
 
-                alert(`Player choose ${playerSelection} and the computer picked ${computerSelection}`);
-                if (playerSelection===computerSelection) {
-                    alert('it\'s a tie! No points awarded');
-                    return 0;
-                }
-                if (
-                    (playerSelection === "ROCK" && computerSelection === "SCISSORS")
-                    ||
-                    (playerSelection === "PAPER" && computerSelection === "ROCK")
-                    ||
-                    (playerSelection === "SCISSORS" && computerSelection === "PAPER"))
-                    { alert ("Player wins the round!");
-                    return 1;
-                } else {
-                    alert ("Computer wins the round!");
-                    return 2;
-                }
-            };
+// determine length of Game
+function lengthOfGame() {
+    if (playerScore > (numOfRounds/2)){
+            scoreDialogue.textContent='Congratulations! You win!';
+            clearScores();
+        } else if (computerScore > (numOfRounds/2)){
+            scoreDialogue.textContent='sorry... you lose.';
+            clearScores();
+        } else {
+        };
+};
 
-        // attribute a score based upon the result
-            function updateScore (result) {
+// reset game upon completion
+function clearScores() {
+    playerScore = 0;
+    computerScore = 0;
+};
 
-                if(result === 0) {
-                    playerScore += 0;
-                    computerScore +=0;
-                };
-                if (result === 1) {
-                    playerScore++;
-                };
-                if (result === 2) {
-                    computerScore++;
-                };
-            };
+// button functionality
+btnRock.addEventListener("click",function(){
+    var playerPlay = 'ROCK';
+    var computerPlay = getComputerSelection();
+    var result = determineWinner(playerPlay,computerPlay);
+    updateScore(result);
+    scoreBoard.textContent=`Player: ${playerScore} Computer: ${computerScore}`;
+    lengthOfGame();
+});
 
-        // clears the scores at the end of the game
-            function clearScores() {
-                playerScore = 0;
-                computerScore = 0;
-            };
+btnPaper.addEventListener("click",function(){
+    var playerPlay = 'PAPER';
+    var computerPlay = getComputerSelection();
+    var result = determineWinner(playerPlay,computerPlay);
+    updateScore(result);
+    scoreBoard.textContent=`Player: ${playerScore} Computer: ${computerScore}`;
+    lengthOfGame();
+});
 
-        playGame(5);
+btnScissors.addEventListener("click",function(){
+    var playerPlay = 'SCISSORS';
+    var computerPlay = getComputerSelection();
+    var result = determineWinner(playerPlay,computerPlay);
+    updateScore(result);
+    scoreBoard.textContent=`Player: ${playerScore} Computer: ${computerScore}`;
+    lengthOfGame();
+});  
+
+// attribute a score based upon the result in the case of a tie or win
+function updateScore (result) {
+    if(result === 0) {
+        playerScore += 0;
+        computerScore +=0;
+    };
+    if (result === 1) {
+        playerScore++;
+    };
+    if (result === 2) {
+        computerScore++;
+    };
+};
+
+// generate a random computer selection
+function getComputerSelection (computerSelection) {
+    var computerSelection = combatOptions[Math.floor(Math.random()*3)];
+    return computerSelection;
+};
+
+// compare the computer and player selections and determine a winner
+function determineWinner (playerSelection, computerSelection) {
+    scoreDialogue.textContent= `Player chose ${playerSelection} and the computer picked ${computerSelection}`;
+    if (playerSelection===computerSelection) {
+        scoreDialogue.textContent= 'it\'s a tie! No points awarded';
+        return 0;
+    }
+    if (
+        (playerSelection === "ROCK" && computerSelection === "SCISSORS")
+        ||
+        (playerSelection === "PAPER" && computerSelection === "ROCK")
+        ||
+        (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
+        return 1;
+    } else {
+        return 2;
+    };
+};
